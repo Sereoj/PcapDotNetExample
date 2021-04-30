b9f234f7d8e7b02308cbd95d721b7e8be70c979d
Sergey Egorov <ser.12egorov@gmail.com>
2021-04-30 15:28:04 +0300

    Проверка файла

--- PcapDotNetExample/Program.cs
+++ PcapDotNetExample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using PcapDotNet.Core;
 using PcapDotNet.Packets;
 using PcapDotNet.Packets.IpV4;
@@ -9,19 +10,21 @@ namespace InterpretingThePackets
 {
     class Program
     {
-
+        private static string fileName = "SV.pcap";
         private static int counter;
         private static string[][] arr = new string[18317][];
         static void Main(string[] args)
         {
-
-            PacketDevice selectedDevice = new OfflinePacketDevice("SV.pcap");
-
-            // Open the device
-            using ( PacketCommunicator communicator = selectedDevice.Open(65536, PacketDeviceOpenAttributes.Promiscuous, 1000) )
+            if( FileCheck(fileName) )
             {
-                communicator.ReceivePackets(0, PacketHandler);
+                PacketDevice selectedDevice = new OfflinePacketDevice(fileName);
+                // Open the device
+                using ( PacketCommunicator communicator = selectedDevice.Open(65536, PacketDeviceOpenAttributes.Promiscuous, 1000) )
+                {
+                    communicator.ReceivePackets(0, PacketHandler);
+                }
             }
+
             Console.WriteLine(arr);
             Console.ReadLine();
         }
@@ -45,7 +48,11 @@ namespace InterpretingThePackets
             //Console.WriteLine("APP ID: {0}", packet.Ethernet.Arp.HardwareType.ToString()); // APP ID
             //Console.WriteLine("MAC SOURCE: {0}", packet.Ethernet.Source.ToString());
             //Console.WriteLine("MAC Destination: {0}", packet.Ethernet.Destination.ToString());
+        }
 
+        public static bool FileCheck(string file)
+        {
+            return File.Exists(file);
         }
     }
 }
